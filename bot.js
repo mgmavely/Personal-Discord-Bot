010254676ad92c9a8e1d7cee12b8ff1fcb7dab89
Michael Mavely <42074665+mgmavely@users.noreply.github.com>
2021-11-12 17:49:44 +0000

    Added bot reacting to messages command and custom bot prefix

--- bot.js
+++ bot.js
@@ -1,11 +1,35 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const token = process.env['token']
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on("messageCreate",  message => {
+  const prefix = "|";
+  console.log(message.content.startsWith(prefix))
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  console.log(args);
+  console.log(command);
+
+  if (command === "ratio" /*message.member.roles.cache.some(r => r.name === "Mod")*/) {
+          if (message.mentions.repliedUser !== null) {
+            message.channel.messages.fetch(message.reference.messageId).then(msg => {msg.react('🇷')
+            msg.react('🇦')
+            msg.react('🇹')
+            msg.react('🇮')
+            msg.react('🇴')}
+            )
+          }          
+        }
+
+});
+
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
